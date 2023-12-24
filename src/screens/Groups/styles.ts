1e2f7c9e76c3cbbf87217e5130e92a96aee8de48
Lucas Gabriel <gabif.johann@gmail.com>
2023-12-24 02:49:39 -0300

    add: using styled components on the element

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #000;
+`
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 32px;
+`
