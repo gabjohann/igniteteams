1b984c7cebe18259118f5f1f19d7efb5cb6eb407
Lucas Gabriel <gabif.johann@gmail.com>
2023-12-26 20:07:35 -0300

    add: listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    'Galera da Rocket',
+    'Galera do Ignite',
+  ])
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
