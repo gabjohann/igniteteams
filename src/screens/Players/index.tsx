53c8f8e20c417ccb7ee9495296390785eb398d79
Lucas Gabriel <gabif.johann@gmail.com>
2023-12-27 03:14:45 -0300

    add: finish player list

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+
 import { Input } from '@components/Input'
+import { Button } from '@components/Button'
 import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { ListEmpty } from '@components/ListEmpty'
 import { ButtonIcon } from '@components/ButtonIcon'
+import { PlayerCard } from '@components/PlayerCard'
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
-import { PlayerCard } from '@components/PlayerCard'
 
 export function Players() {
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState(['Lucas'])
+  const [players, setPlayers] = useState([])
 
   return (
     <Container>
@@ -50,7 +53,17 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={players} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   )
 }
