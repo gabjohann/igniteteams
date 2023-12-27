8c455efbec3372486fb4218f7610050c7c4ced20
Lucas Gabriel <gabif.johann@gmail.com>
2023-12-27 02:18:55 -0300

    feat: implementation for the filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
 import { Input } from '@components/Input'
+import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { ButtonIcon } from '@components/ButtonIcon'
@@ -18,6 +19,8 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <Filter title="Time A" />
     </Container>
   )
 }
