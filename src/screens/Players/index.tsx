bdfe52ceb560429454311efda353aac97a57216a
Lucas Gabriel <gabif.johann@gmail.com>
2023-12-29 00:35:00 -0300

    feat: passing parameters through routes

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+import { useRoute } from '@react-navigation/native'
 
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
@@ -12,18 +13,22 @@ import { PlayerCard } from '@components/PlayerCard'
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
 
+type RouteParams = {
+  group: string
+}
+
 export function Players() {
   const [team, setTeam] = useState('Time A')
   const [players, setPlayers] = useState([])
+
+  const route = useRoute()
+  const { group } = route.params as RouteParams
 
   return (
     <Container>
       <Header showBackButton />
 
-      <Highlight
-        title="Nome da turma"
-        subtitle="adicione a galera e separe os times"
-      />
+      <Highlight title={group} subtitle="adicione a galera e separe os times" />
 
       <Form>
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
