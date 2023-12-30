f87f05edf913712df68e17a7cd72e2ff09bef354
Lucas Gabriel <gabif.johann@gmail.com>
2023-12-30 17:48:40 -0300

    feat: save groups on storage

---
+++ src/storage/group/groupCreate.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-useless-catch */
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { GROUP_COLLECTION } from '@storage/storageConfig'
+
+import { getAllGroups } from './getAllGroups'
+
+export async function groupCreate(newGroupName: string) {
+  try {
+    const storedGroups = await getAllGroups()
+
+    const storage = JSON.stringify([...storedGroups, newGroupName])
+
+    await AsyncStorage.setItem(GROUP_COLLECTION, storage)
+  } catch (error) {
+    throw error
+  }
+}
